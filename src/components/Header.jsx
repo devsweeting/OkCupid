b46702dc397d368dc1e8f2b43a4c5edbe4f12d5a
Devin Sweeting and Paige Williams <paige@>
2019-02-13 13:32:24 -0800

    Add styles for buttons in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,14 +11,28 @@ const Title = styled.h1`
 const Wrapper = styled.section`
   padding: 4em;
   background: #fec5e2;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TestButton = styled(Link)`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  text-decoration: none;
+  color: palevioletred;
 `;
 
 function Header(){
   return (
     <Wrapper>
       <Title>Okcupid</Title>
+        <div>
       <h3>Have an account?</h3>
-      <Link to="/">Home</Link> | <Link to="/signin">Sign In</Link>
+      <TestButton to="/">Home</TestButton>  <TestButton to="/signin">Sign In</TestButton>
+      </div>
     </Wrapper>
   );
 }
